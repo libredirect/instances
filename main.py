libredirect/instances

commit 493675e32fd2651c1270b6c4b3f545c1b34562c9

Chore

main.py
@@ -21,9 +21,7 @@ cloudtube(mightyList)
 proxitok(mightyList)
 send(mightyList)
 nitter(mightyList)
-libreddit(mightyList)
 redlib(mightyList)
-teddit(mightyList)
 scribe(mightyList)
 quetre(mightyList)
 libremdb(mightyList)
@@ -33,7 +31,6 @@ libreTranslate(mightyList)
 searxng(mightyList)
 searx(mightyList)
 whoogle(mightyList)
-librex(mightyList)
 rimgo(mightyList)
 pixivFe(mightyList)
 safetwitch(mightyList)
@@ -52,10 +49,8 @@ biblioReads(mightyList)
 suds(mightyList)
 poketube(mightyList)
 gothub(mightyList)
-mikuInvidious(mightyList)
 wolfreeAlpha(mightyList)
-jiti(mightyList)
-proxigram(mightyList)
+jitsi(mightyList)
 tent(mightyList)
 laboratory(mightyList)
 twineo(mightyList)

services/from_file.py
@@ -13,10 +13,6 @@ def wikiless(mightyList):
     fetchFromFile('wikiless', mightyList)
 
 
-def mikuInvidious(mightyList):
-    fetchFromFile('mikuInvidious', mightyList)
-
-
 def twineo(mightyList):
     fetchFromFile('twineo', mightyList)
 

services/other.py
@@ -20,7 +20,7 @@ def tent(mightyList):
 def ruralDictionary(mightyList):
     fetchJsonList(
         'ruralDictionary',
-        'https://codeberg.org/zortazert/rural-dictionary/raw/branch/master/instances.json',
+        'https://codeberg.org/cobra/rural-dict/raw/branch/main/instances.json',
         {
             'clearnet': 'clearnet',
             'tor': 'tor',
@@ -28,15 +28,6 @@ def ruralDictionary(mightyList):
             'loki': None
         },
         False,
-        mightyList
-    )
-
-
-def laboratory(mightyList):
-    fetchRegexList(
-        'laboratory',
-        'https://git.vitali64.duckdns.org/utils/laboratory.git/plain/README.md',
-        r"\| (https:\/{2}.*?) \|",
         mightyList
     )
 
@@ -164,7 +155,7 @@ def rimgo(mightyList):
         logging.error(traceback.format_exc())
 
 
-def jiti(mightyList):
+def jitsi(mightyList):
     fetchRegexList(
         'jitsi',
         "https://raw.githubusercontent.com/jitsi/handbook/master/docs/community/instances.md",
@@ -330,7 +321,6 @@ def skunkyArt(mightyList):
     except Exception:
         fetchCache('skunkyArt', mightyList)
         logging.error(traceback.format_exc())
-
 
 def koub(mightyList):
     fetchJsonList(

services/reddit.py
@@ -1,21 +1,6 @@
 from .utils import fetchJsonList
 
 
-def libreddit(mightyList):
-    fetchJsonList(
-        'libreddit',
-        'https://github.com/libreddit/libreddit-instances/raw/master/instances.json',
-        {
-            'clearnet': 'url',
-            'tor': 'onion',
-            'i2p': 'i2p',
-            'loki': None
-        },
-        True,
-        mightyList
-    )
-
-
 def redlib(mightyList):
     fetchJsonList(
         'redlib',
@@ -27,20 +12,5 @@ def redlib(mightyList):
             'loki': None
         },
         True,
-        mightyList
-    )
-
-
-def teddit(mightyList):
-    fetchJsonList(
-        'teddit',
-        'https://codeberg.org/teddit/teddit/raw/branch/main/instances.json',
-        {
-            'clearnet': 'url',
-            'tor': 'onion',
-            'i2p': 'i2p',
-            'loki': None
-        },
-        False,
         mightyList
     )

services/search.py
@@ -54,18 +54,3 @@ def whoogle(mightyList):
         r"\| \[https?:\/{2}(?:[^\s\/]+\.)*(?:[^\s\/]+\.)+[a-zA-Z0-9]+\]\((https?:\/{2}(?:[^\s\/]+\.)*(?:[^\s\/]+\.)+[a-zA-Z0-9]+)\/?\) \| ",
         mightyList
     )
-
-
-def librex(mightyList):
-    fetchJsonList(
-        'librex',
-        'https://raw.githubusercontent.com/hnhx/librex/main/instances.json',
-        {
-            'clearnet': 'clearnet',
-            'tor': 'tor',
-            'i2p': 'i2p',
-            'loki': None
-        },
-        True,
-        mightyList
-    )

services/translate.py
@@ -27,3 +27,12 @@ def mozhi(mightyList):
         False,
         mightyList
     )
+
+def simplytranslate(mightyList):
+        fetchJsonList(
+        'simplytranslate',
+        'https://codeberg.org/ManeraKai/simplytranslate/raw/branch/main/instances.json',
+        'link',
+        False,
+        mightyList
+    )

commit 55ba0d9b911adb6aa37aab5c1bda1fabc4c6b53a

Readd proxigram to main.py
No instances are found as we never look for them after the call was
presumably dropped accidentally a (long) while ago.

Fixes: 493675e32fd2651c1270b6c4b3f545c1b34562c9

main.py
@@ -19,6 +19,7 @@ piped(mightyList)
 pipedMaterial(mightyList)
 cloudtube(mightyList)
 proxitok(mightyList)
+proxigram(mightyList)
 send(mightyList)
 nitter(mightyList)
 redlib(mightyList)